a38440c16dcbe5cd8e6c7044337eca8c6afc5cc6
Andrew Qi <andrew99qi@gmail.com>
2021-01-27 23:01:23 -0800

    p2

---
+++ image.js
@@ -0,0 +1,11 @@
+$("#imageB").click(on){
+    document.getElementById("overlay").style.display = "block";
+}
+document.getElementById("imageA").addEventListener("click", on)
+function onA() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  } 
